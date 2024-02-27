5b9c54a94f602b9fc412bce183f8ae9c0528cd3b
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-02-27 19:13:02 +0300

    make migrations

--- database/migrations/2024_02_27_160916_create_prayerrequest_table.php
+++ database/migrations/2024_02_27_160916_create_prayerrequest_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('prayerrequest', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('prayer_id');
-            $table->bigInteger('user_id');
+            $table->unsignedBigInteger('prayer_id');
+            $table->unsignedBigInteger('user_id');
             $table->enum('status',['Pending','Answered']);
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('prayer_id')->references('id')->on('prayer')->onDelete('restrict')->onUpdate('cascade');
         });
     }
 
