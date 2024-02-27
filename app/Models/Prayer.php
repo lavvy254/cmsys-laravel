90e20ad1f0e28eadd3f95901debcb242a358e73d
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-02-27 19:20:11 +0300

    create relationship,make model

--- app/Models/Prayer.php
+++ app/Models/Prayer.php
@@ -13,4 +13,14 @@ class Prayer extends Model
         'title',
         'description'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function prayer()
+    {
+        return $this->belongsTo(Prayer::class);
+    }
 }
