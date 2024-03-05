48667bd07dfb16b3d962d8fdf95e6291501330e2
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-03-05 20:38:19 +0300

    Update GivingController.php

--- app/Http/Controllers/GivingController.php
+++ app/Http/Controllers/GivingController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Giving;
 
 class GivingController extends Controller
 {
     public function index()
     {
-        return view('pages.giving.give');
+        $givings = Giving::all();
+        return view('pages.giving.give',compact('givings'));
     }
 }
