d72f14b53c84b0f05bf948dee04e9e4384bd749d
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-03-05 20:35:21 +0300

    Update Giving.php

--- app/Models/Giving.php
+++ app/Models/Giving.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Giving extends Model
 {
-    use HasFactory;
+    protected $fillable =[
+        'user_id',
+        'type',
+        'transaction',
+        'amount',
+        'description',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
