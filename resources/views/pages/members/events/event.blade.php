09ab6a538f0f4fd119c903d84bcc66b451b80df0
lavvy254 <wangecimaina254@gmail.com>
2024-03-13 10:28:04 +0300

    Update event.blade.php

--- resources/views/pages/members/events/event.blade.php
+++ resources/views/pages/members/events/event.blade.php
@@ -18,109 +18,35 @@
                   ID
                 </th>
                 <th>
-                  Title
+                  Event Name
                 </th>
                 <th>
-                  Description
+                  Location
                 </th>
-                <th class="text-center">
-                  
-                </th>
+                <th>
+                   Description
+                  </th>
               </tr>
             </thead>
-            @foreach ($prayers as $prayer)
+            @foreach ($events as $event)
             <tbody>
               <tr>
                 <td>
-                  {{$prayer->id}}
+                  {{$event->id}}
                 </td>
                 <td>
-                  {{$prayer->title}}
+                  {{$event->ename}}
                 </td>
                 <td>
-                  {{$prayer->description}}
-                </td>
-                  <td class="text-center">
-                        <div class="dropdown">
-                          <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              <i class="fas fa-ellipsis-v"></i>
-                          </a>
-                          <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                            <a class="dropdown-item" href="{{route('prayer.edit',$prayer->id)}}">Edit</a>
-                            <form action="{{route('prayer.delete',$prayer->id)}}" method="POST">
-                                  @csrf
-                                  @method('DELETE')
-                                  <button type="submit" class="dropdown-item" >Delete</button>
-                            </form>
-                          </div>
-                      </div>
+                    {{$event->location}}
+                  </td>
+                <td>
+                  {{$event->description}}
                 </td>
               </tr>
             </tbody>
             @endforeach
-            {{ $prayers->links('vendor.pagination.bootstrap-5') }}
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-12">
-    <div class="card  card-plain">
-      <div class="card-header">
-        <h4 class="card-title"> REQUESTED PRAYES</h4>
-        <p class="category"> Here is a table for prayer requests</p>
-      </div>
-      <div class="card-body">
-        <div class="table-responsive">
-          <table class="table tablesorter " id="">
-            <thead class=" text-primary">
-              <tr>
-                <th>
-                  ID
-                </th>
-                <th>
-                  Prayer Name
-                </th>
-                <th>
-                  First Name
-                </th>
-                <th>
-                  Status
-                </th>
-                <th class="text-center"></th>
-                
-              </tr>
-            </thead>
-            @foreach ($prayerrequests as $prayerrequest)
-            <tbody>
-              <tr>
-                <td>
-                  {{$prayerrequest->id}}
-                </td>
-                <td>
-                  {{$prayerrequest->Prayer->title}}
-                </td>
-                <td>
-                  {{$prayerrequest->User->fname}}
-                </td>
-                <td>
-                  {{$prayerrequest->status}}
-                </td>
-                </td>
-                  <td class="text-center">
-                        <div class="dropdown">
-                          <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              <i class="fas fa-ellipsis-v"></i>
-                          </a>
-                          <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                            <a class="dropdown-item" href="{{route('prayerrequest.edit',$prayerrequest->id)}}">Edit</a>
-                          </div> 
-              <td class="text-center">
-
-              </td>
-            </tbody>
-             @endforeach
-             {{ $prayerrequests->links('vendor.pagination.bootstrap-5') }}
+            {{ $event->links('vendor.pagination.bootstrap-5') }}
           </table>
         </div>
       </div>
