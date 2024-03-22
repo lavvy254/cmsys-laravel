af3f38e007a9c1cc3c971fb7f0d82e4cf6c08b04
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-03-22 12:04:14 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Application Service Providers...
          */
+        \Safaricom\Mpesa\MpesaServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
@@ -185,6 +186,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Mpesa'=> \Safaricom\Mpesa\MpesaServiceProvider::class,
     ])->toArray(),
 
 ];
