93d51c5fabb426e42d17e9666709422e026d2568
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-03-26 12:32:13 +0300

    Update edit.blade.php

--- resources/views/pages/prayer/prayer_request/edit.blade.php
+++ resources/views/pages/prayer/prayer_request/edit.blade.php
@@ -14,7 +14,7 @@
             @method('PUT')
             <div class="col-md-6 mb-3">
              <label for="prayer" class="form-label">Prayer Name</label>
-                <select class="form-select" name="prayer_id" id="prayer_id">
+                <select class="form-control bg-dark" name="prayer_id" id="prayer_id">
                  <option value="">Select Prayer</option>
                      @foreach ($prayers as $prayer)
                       <option value="{{ $prayer->id }}">{{ $prayer->title }}</option>
@@ -26,7 +26,7 @@
             </div>
             <div class="col-md-6 mb-3">
              <label for="users" class="form-label">Full Name</label>
-                <select class="form-select " name="user_id" id="user_id">
+                <select class="form-control bg-dark " name="user_id" id="user_id">
                  <option value="">Select Users</option>
                      @foreach ($users as $user)
                       <option value="{{ $user->id }}">{{ $user->fname }}</option>
@@ -36,10 +36,15 @@
                  <span class="text-danger">{{ $message }}</span>
                 @enderror
             </div>
-            <div class=" col-md-6 mb-3">
-                <label for="status" class="form-label">status</label>
-                <input type="text" class="form-control" id="status" name="status" value="{{ $prayer_request->status }}">
+            <div class="col-md-6 mb-3">
+               <select class="form-control bg-dark" name="status" id="status">
+                 <option value="pending">Pending</option>
+                 <option value="Answered">Answered</option>
+              </select>
             </div>
+            @error('status')
+             <span class="text-danger">{{ $message}}</span>
+            @enderror
             </div>
             <button type="submit" class="btn btn-primary btn-sm">Submit</button>
         
