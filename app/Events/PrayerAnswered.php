b56210ccd46e2db6bf48832582aa1ddc37d6e9fe
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-03-26 11:31:44 +0300

    Create PrayerAnswered.php

---
+++ app/Events/PrayerAnswered.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PrayerAnswered
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+}
